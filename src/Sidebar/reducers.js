a208d666e9ecd08b45110910ec417f7e1d695399
Nima Mehanian <nima.mehanian@gmail.com>
2016-12-24 17:39:36 -0800

    [wip] Make Sidebar component

---
+++ src/Sidebar/reducers.js
@@ -0,0 +1,56 @@
+const initialState = {
+  isOpen: false,
+  isPeeking: false,
+  activeIndex: 0,
+  links: [
+    { name: 'home', isActive: true },
+    { name: 'about', isActive: false },
+    { name: 'services', isActive: false },
+    { name: 'work', isActive: false },
+    { name: 'blog', isActive: false },
+    { name: 'connect', isActive: false }
+  ]
+};
+
+import {
+  SIDEBAR_OPEN,
+  SIDEBAR_SHUT,
+  SIDEBAR_PEEK,
+  SIDEBAR_HIDE,
+  SIDEBAR_LINK_SELECT
+} from './actionTypes';
+
+const links = (links = [], index) => {
+  let reset = links.map(link => (
+    { name: link.name, isActive: false }
+  ));
+
+  return [
+    ...reset.slice(0, index),
+    { name: reset[index].name, isActive: true },
+    ...reset.slice(index + 1)
+  ];
+};
+
+const sidebar = (state = initialState, action) => {
+  switch (action.type) {
+    case SIDEBAR_OPEN:
+      return { ...state, isOpen: true };
+    case SIDEBAR_SHUT:
+      return { ...state, isOpen: false };
+    case SIDEBAR_PEEK:
+      return { ...state, isPeeking: true };
+    case SIDEBAR_HIDE:
+      return { ...state, isPeeking: false };
+    case SIDEBAR_LINK_SELECT:
+      return {
+        ...state,
+        activeIndex: action.index,
+        links: links(state.links, action.index)
+      };
+    default:
+      return state;
+  }
+};
+
+export default sidebar;
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -4,4 +4,10 @@ const config = {
 
 };
 
+if (process.env.NODE_ENV === 'production') {
+  console.log('In production mode:');
+} else {
+  console.log('In development mode:');
+}
+
 export default config;
